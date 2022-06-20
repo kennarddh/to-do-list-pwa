3cffde6340c88a88d47c846d32b1432957013310
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-20 12:33:37 +0700

    feat: Service worker

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = 'to-do-list-pwa'
+const filesToCache = ['/', '/index.html', '/css/style.css', '/js/main.js']
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+	)
+})
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(response => response || fetch(event.request))
+	)
+})
