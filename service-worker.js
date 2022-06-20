25ed2e2f3d9a33e4e8bbcbc16cb2df93840164b3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-20 16:32:23 +0700

    fix: Remove is github

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,22 @@
 const cacheName = 'to-do-list-pwa'
 
-const version = '1.0.1'
+const version = '1.0.2'
 
-const isGithubPage = true
 const repoName = 'to-do-list-pwa'
 
-const filesToCacheOriginal = [
-	'/',
-	'/index.html',
-	'/css/style.css',
-	'/js/main.js',
+const filesToCache = [
+	`${repoName}/`,
+	`${repoName}/index.html`,
+	`${repoName}/css/style.css`,
+	`${repoName}/js/main.js`,
 ]
-
-const filesToCache = isGithubPage
-	? filesToCacheOriginal.map(element => `${repoName}${element}`)
-	: filesToCacheOriginal
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', event => {
 	event.waitUntil(
-		caches.open(`${cacheName}-${version}`).then(cache => {
-			cache.addAll(filesToCache).then(console.log)
-		})
+		caches
+			.open(`${cacheName}-${version}`)
+			.then(cache => cache.addAll(filesToCache))
 	)
 })
 
