e6a53a443e8e64aa4e2a160a9a53ca27d9af62b9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-20 15:10:59 +0700

    feat: Add task

--- js/main.js
+++ js/main.js
@@ -6,11 +6,31 @@ window.addEventListener('load', () => {
 	}
 })
 
-const AddForm = document.querySelector('#add-form')
-const AddInput = document.querySelector('#add-form #task')
+const addForm = document.querySelector('#add-form')
+const addInput = document.querySelector('#add-form #task')
+const taskTemplate = document.querySelector('template#taskRow')
+const taskList = document.querySelector('#tasks tbody')
 
 const randomId = () => {
 	const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0]
 
 	return uint32.toString(16)
 }
+
+addForm.addEventListener('submit', event => {
+	event.preventDefault()
+
+	const task = addInput.value
+
+	if (!addInput.value) return alert('Please enter a task')
+
+	const id = randomId()
+
+	const taskElement = taskTemplate.content.cloneNode(true)
+
+	taskElement.querySelector('#task').textContent = task
+
+	taskElement.querySelector('.delete').setAttribute('data-id', id)
+
+	taskList.appendChild(taskElement)
+})
